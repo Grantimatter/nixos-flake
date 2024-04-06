0bd065fafcb53ccece560c8f2e3cca86fcfedfd9
Grant Wiswell <wiswellgrant@gmail.com>
2024-04-05 22:34:49 -0500

    Added laptop os config

---
+++ nixos-configurations/acer-laptop/default.nix
@@ -0,0 +1,55 @@
+{ config, inputs, lib, modulesPath, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../nvidia.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  boot.loader.systemd-boot.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
+
+  services.tailscale.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    desktopManager.xterm.enable = false;
+    excludePackages = [ pkgs.xterm ];
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  networking = {
+    hostName = "acer-laptop";
+    networkmanager.enable = true;
+  };
+
+  nix = {
+    extraOptions = "experimental-features = nix-command flakes";
+    settings.trusted-users = [
+      "root"
+    ];
+
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  environment.systemPackages = lib.attrValues {
+    inherit (pkgs)
+      git
+      neovim
+      ;
+  };
+
+  system.stateVersion = "23.11";
+}
