64a2994f67d0053b90638eeb27682cf11283f606
Grant Wiswell <wiswellgrant@gmail.com>
2025-05-06 09:19:40 -0500

    Ghosty Resize keybinds

--- home-modules/ghostty.nix
+++ home-modules/ghostty.nix
@@ -9,6 +9,12 @@
       theme = "catppuccin-mocha";
       shell-integration = "fish";
       font-family = "FiraCode Nerd Font Mono";
+      keybind = [
+        "alt+ctrl+u=resize_split:down,40"
+        "alt+ctrl+y=resize_split:up,40"
+        "alt+ctrl+l=resize_split:left,40"
+        "alt+ctrl+apostrophe=resize_split:right,40"
+      ];
     };
   };
 }
