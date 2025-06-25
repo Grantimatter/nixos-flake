eb103a93d4d0a43166d9c3133ced55c904dee1e0
Grant Wiswell <wiswellgrant@gmail.com>
2025-06-25 16:15:40 -0500

    Gaming laptop changes

---
+++ nixos-configurations/gaming-laptop/default.nix
@@ -0,0 +1,90 @@
+{ config, inputs, lib, modulesPath, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../nvidia.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+  
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+#  boot.loader.systemd-boot.enable = true;
+  boot.plymouth.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+
+  programs.steam.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
+
+  services.tailscale.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    desktopManager.xterm.enable = false;
+#    displayManager.lightdm.enable = true;
+#    displayManager.defaultSession = "none+i3";
+#    windowManager.i3.enable = true;
+
+    excludePackages = [ pkgs.xterm ];
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  networking = {
+    hostName = "gaming-laptop";
+    networkmanager.enable = true;
+  };
+
+  nix = {
+    extraOptions = "experimental-features = nix-command flakes";
+    settings.trusted-users = [
+      "root"
+    ];
+
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  users.users.chloe = {
+    isNormalUser = true;
+    home = "/home/chloe";
+    description = "Chloe Wiswell";
+  };
+
+  hardware.nvidia = {
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+      sync.enable = true;
+    };
+  };
+
+  environment.systemPackages = lib.attrValues {
+    inherit (pkgs)
+      git
+      moonlight-qt
+      firefox
+      wezterm
+      #wowup-cf
+      discord
+      r2modman
+      deluge
+      ;
+  };
+
+  system.stateVersion = "23.11";
+}
