5f179ff04b9a6499f1790fdf14c7b8d8de2e748a
Grant Wiswell <wiswellgrant@gmail.com>
2025-08-17 07:13:22 -0500

    Default browser settings added

--- home-configurations/grant.nix
+++ home-configurations/grant.nix
@@ -25,5 +25,18 @@
     lazyjj
   ];
 
+  home.users.grant = {
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "zen.desktop";
+        "x-scheme-handler/http" = "zen.desktop";
+        "x-scheme-handler/https" = "zen.desktop";
+        "x-scheme-handler/about" = "zen.desktop";
+        "x-scheme-handler/unknown" = "zen.desktop";
+      };
+    };
+  };
+  
   programs.nnn.enable = true;
 }
